70755d57c1aa3d7e0478e72fa012d21266dc8524
diawando <abduldiawade777@gmail.com>
2023-03-11 01:35:36 +0000

    mise à jour de init_db.py

--- init_db.py
+++ init_db.py
@@ -1,6 +1,9 @@
-from app import db, Post, Comment
+from app import db, Post, Comment, Tag
 from app import app
 with app.app_context():
+    db.drop_all()
+    db.create_all()
+    
     post1 = Post(title='Post The First', content='Content for the first post')
     post2 = Post(title='Post The Second', content='Content for the Second post')
     post3 = Post(title='Post The Third', content='Content for the third post')
@@ -9,8 +12,21 @@ with app.app_context():
     comment2 = Comment(content='Comment for the second post', post=post2)
     comment3 = Comment(content='Another comment for the second post', post_id=2)
     comment4 = Comment(content='Another comment for the first post', post_id=1)
+    
+    tag1 = Tag(name='animals')
+    tag2 = Tag(name='tech')
+    tag3 = Tag(name='cooking')
+    tag4 = Tag(name='writing')
+    
+    post1.tags.append(tag1) # Tag the first post with 'animals'
+    post1.tags.append(tag4) # Tag the first post with 'writing'
+    post3.tags.append(tag3) # Tag the third post with 'cooking'
+    post3.tags.append(tag2) # Tag the third post with 'tech'
+    post3.tags.append(tag4) # Tag the third post with 'writing'
+    
 
     db.session.add_all([post1, post2, post3])
     db.session.add_all([comment1, comment2, comment3, comment4])
+    db.session.add_all([tag1, tag2, tag3, tag4])
 
     db.session.commit()
