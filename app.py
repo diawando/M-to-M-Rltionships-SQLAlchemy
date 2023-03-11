ec285e9d08cea50203902d33fa504288d4082f90
diawando <abduldiawade777@gmail.com>
2023-03-11 01:20:56 +0000

    creating associate table, to establish the multi to multi relationship between table post and tag

--- app.py
+++ app.py
@@ -9,12 +9,26 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+post_tag = db.Table('post_tag',
+                    db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+                    db.Column('tag_id', db.Integer, db.ForeingKey('tag.id'))
+                    )
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    
+    def __repr__(self):
+        return f'<Tag "{self.name}">'
+    
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     content = db.Column(db.Text)
     comments = db.relationship('Comment', backref='post')
+    tags = db.relationship('Tag', secondary=post_tag, backref="posts")
     
     def __repr__(self):
         return f'<Post "{self.title}">'
